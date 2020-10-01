31cc7a1db639cadeca8f8dfa18843aa324c59d55
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-01 14:31:13 +0530

    [Umesh]Added: made a function which gives happy and sad mood

--- src/main/java/com/practice/MoodAnalyser.java
+++ src/main/java/com/practice/MoodAnalyser.java
@@ -3,5 +3,15 @@ package com.practice;
 public class MoodAnalyser {
 	public static void main(String[] args) {
 		System.out.println("Welcome to mood analyser problem");
+		
+	}
+	
+	public String analyseMood(String message){
+		if(message.contains("sad")){
+			return "SAD";
+		}
+		else{
+			return "HAPPY";
+		}
 	}
 }
--- src/test/java/com/practice/MoodTest.java
+++ src/test/java/com/practice/MoodTest.java
@@ -1,5 +1,14 @@
 package com.practice;
 
+import org.junit.*;
+
+
 public class MoodTest {
 	
+	@Test
+	public void givenMessage_WhenSad_shouldGiveSad(){
+		MoodAnalyser validator = new MoodAnalyser();
+		String result = validator.analyseMood("This is a sad message");
+		Assert.assertEquals("SAD",result);
+	}
 }
