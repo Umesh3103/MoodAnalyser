95eb28bb59f16436db0aa7f48a892bc39cc9638e
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-01 18:41:13 +0530

    [Umesh]Added: catching emptyString exception

--- src/test/java/com/practice/MoodTest.java
+++ src/test/java/com/practice/MoodTest.java
@@ -5,7 +5,7 @@ import org.junit.*;
 public class MoodTest {
 	
 	@Test
-	public void givenMessage_WhenSad_shouldGiveSad(){
+	public void givenMessage_WhenNull_shouldGiveNullMessage(){
 		String result;
 		try{
 		MoodAnalyser validator = new MoodAnalyser(null);
@@ -16,6 +16,21 @@ public class MoodTest {
 			System.out.println(e.exceptionType);
 			Assert.assertEquals("Enter proper message", e.getMessage());
 			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.exceptionType);
+		}
+		
+	}
+	@Test
+	public void givenMessage_WhenEmpty_shouldGiveEmptyMessage(){
+		String result;
+		try{
+		MoodAnalyser validator = new MoodAnalyser("");
+		result = validator.analyseMood();
+		Assert.assertEquals("SAD",result);
+		}
+		catch(MoodAnalyserException e){
+			System.out.println(e.exceptionType);
+			Assert.assertEquals("Enter proper message", e.getMessage());
+			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.exceptionType);
 		}
 		
 	}
